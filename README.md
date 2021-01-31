70a0826b57ad7c94467b763b14d3f022a9c5a69b
OverAcheiever <75264797+OverAcheiever@users.noreply.github.com>
2021-01-31 22:28:39 +0530

    v1
    Added Readme and License

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# StreamElements-ChatBot-Rally
- StreamElements Bot which displays information from the Rally API on commands
+# StreamElements ChatBot for Rally
+StreamElements ChatBot which listens to specific commands, and displays public Rally Information in chat through the StreamElements Bot.
+
+# Installation
+1. Click [here](https://streamelements.com/dashboard/overlays/share/5fd6148e4d2bf41f92bc49e1) to install the ChatBot widget to your StreamElements Overlays.
+2. Getting a JWT Token (Used to send messages via SE Bot):
+   - Open [Jebaited](https://jebaited.net) 
+   - Click Sign-In with StreamElements and complete the Sign-In
+   - After a sucessful Sign-In, click [here](https://jebaited.net/tokens/) to open the Token Dashboard
+   - Click the Scopes button and check **botMsg**, then click **Add Token** 
+     ![alt test](https://media.discordapp.net/attachments/766155327582109716/805471127810867210/unknown.png?width=886&height=498)
+   - The token should look something like this. 
+   ![alt text](https://media.discordapp.net/attachments/766155327582109716/805472255998951475/unknown.png?width=1025&height=166)
+   - Copy the token, and open the ChatBot widget in the StreamElements overlay editor.
+   - In the overlay editor, Navigate to **Settings -> JWT Token** and paste in the copied JWT Token.
+
+        ![alt text](https://media.discordapp.net/attachments/766155327582109716/805481216307625984/unknown.png)
+   - Navigate to **Settings -> CCoin** and enter your Creator Coin Symbol
+   - Save the overlay (top right).
+3. Click the url icon on the top-right of the menu bar to copy the widget url.![alt text](https://media.discordapp.net/attachments/766155327582109716/805468910862204928/unknown.jpg)
+4. Add the ChatBot widget in **Every Scene** in your OBS as a **Browser Source**. The ChatBot is activated only when it is active in OBS.
+
+# Getting Started
+## **Default Commands**
+- **Commands List:** !rlycmds
+- **Rally Price:** !rly
+- **Creator Coin Information:** !cc
+- **Creator Coin Transactions:** !cct
+- **Creator Coin Supporters:** !ccs 
+- **Creator Coin Support Volume:** !ccsv
+- **Creator Coin Count:** !ccc
+
+![alt text](https://media.discordapp.net/attachments/766155327582109716/805481751659937792/unknown.png)
+## **Default Command Cooldown**
+The Default Command Cooldown is **10 seconds**.
+
+## **Customisation**
+Each Command offers customisation of the command name (Eg: **!rly**) and Command Cooldown
+
+![alt text](https://media.discordapp.net/attachments/766155327582109716/805477323943706644/unknown.png)
+
+1. To disable a command, leave the command name field empty
+2. The Command Cooldown is to be entered in seconds
+
+## **Made With:**
+- HTML
+- CSS
+- JavaScript
+- JSON
+
+## **Developer Information**
+This widget is made by OverAchiever.
+- Discord: **OverAchiever#7428**
+- [Twitch](https://www.twitch.tv/overacheiever)
+
+**This Project is under an MIT License. License can be found [here](https://github.com/OverAcheiever/StreamElements-ChatBot-Rally/blob/main/LICENSE.md)**
--- app.js
+++ app.js
@@ -1,146 +1,139 @@
-let fieldData = {};
-let callBack = {};
-let rlyprice = "";
-let rallyPriceCommandCooldown = false;
-let creatorCoinSymbolCooldown = false;
-let creatorCoinPriceCommandCooldown = false;
-let creatorCoinCooldown = false;
-let creatorCoinTransactionsCommandCooldown = false;
-let creatorCoinSupportersCommandCooldown = false;
-let creatorCoinSupportVolumeCommandCooldown = false;
-let creatorCoinCountCommandCooldown = false;
+let fieldData = {},
+    callBack = {},
+    commands = !1,
+    rallyPriceCommandCooldown = !1,
+    creatorCoinSymbolCooldown = !1,
+    creatorCoinPriceCommandCooldown = !1,
+    creatorCoinCooldown = !1,
+    creatorCoinTransactionsCommandCooldown = !1,
+    creatorCoinSupportersCommandCooldown = !1,
+    creatorCoinSupportVolumeCommandCooldown = !1,
+    creatorCoinCountCommandCooldown = !1;
 
-
-window.addEventListener('onWidgetLoad', function(obj) {
-    fieldData = obj["detail"]["fieldData"];
+window.addEventListener("onWidgetLoad", function(o) {
+    fieldData = o.detail.fieldData
 });
 
-const sayMessage = (message) => {
-  	let jebaitedAPIToken = fieldData.JWTToken;
-    if (jebaitedAPIToken.length !== 24) return;
-    fetch(`https://api.jebaited.net/botMsg/${jebaitedAPIToken}/`, {
-            method: 'post',
-            body: JSON.stringify({
-                "message": message
-            })
+const sayMessage = o => {
+    let t = fieldData.JWTToken;
+    24 === t.length && fetch(`https://api.jebaited.net/botMsg/${t}/`, {
+        method: "post",
+        body: JSON.stringify({
+            message: o
         })
-        .catch(e => console.error(`Error sending message to chat`));
+    }).catch(o => console.error("Error sending message to chat"))
 };
 
-window.addEventListener('onEventReceived', function(obj) {
-    const listener = obj.detail.listener;
-    const data = obj["detail"]["event"];
-    if (listener != "message") {
-        return;
-    }
-    console.log(obj.detail.event.data.text);
+window.addEventListener("onEventReceived", function(o) {
+    const t = o.detail.listener;
+    o.detail.event;
 
-    if (obj.detail.event.data.text == fieldData.rallyPriceCommand && rallyPriceCommandCooldown == false) {
-        rallyPriceCommandCooldown = true;
-        const Http = new XMLHttpRequest();
-        Http.open("GET", "https://api.coingecko.com/api/v3/coins/rally-2?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false");
-        Http.send();
-
-        Http.onreadystatechange = (e) => {
-            if (Http.readyState == 4 && Http.status == 200) {
-                let data = JSON.parse(Http.responseText);
-                console.log(data.market_data.current_price.usd);
-                sayMessage('Rally Price: $' + String(data.market_data.current_price.usd));
-                setTimeout(function(){rallyPriceCommandCooldown = false;},{rallyPriceCommandCooldown}*1000);
-            }
+    if ("message" == t) {
+        if (o.detail.event.data.text == fieldData.commands && 0 == commands && (commands = !0, sayMessage(`Rally Price: ${fieldData.rallyPriceCommand}, Creator Coin Information: ${fieldData.creatorCoin}, Creator Coin Transactions: ${fieldData.creatorCoinTransactionsCommand}, Creator Coin Supporters: ${fieldData.creatorCoinSupportersCommand}, Creator Coin Support Volume: ${fieldData.creatorCoinSupportVolumeCommand}, Creator Coin Count: ${fieldData.creatorCoinCountCommand}`), setTimeout(function() {
+                commands = !1
+            }, 1e3 * fieldData.commandsCooldown)), o.detail.event.data.text == fieldData.rallyPriceCommand && 0 == rallyPriceCommandCooldown) {
+            rallyPriceCommandCooldown = !0;
+            const o = new XMLHttpRequest;
+            o.open("GET", "https://api.coingecko.com/api/v3/coins/rally-2?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false"), o.send(), o.onreadystatechange = (t => {
+                if (4 == o.readyState && 200 == o.status) {
+                    let t = JSON.parse(o.responseText);
+                    sayMessage("Rally Price: $" + String(t.market_data.current_price.usd)), setTimeout(function() {
+                        rallyPriceCommandCooldown = !1
+                    }, 1e3 * {
+                        rallyPriceCommandCooldown: rallyPriceCommandCooldown
+                    })
+                }
+            })
         }
-    }
-    if (obj.detail.event.data.text == fieldData.creatorCoinPriceCommand && creatorCoinPriceCommandCooldown == false) {
-        creatorCoinPriceCommandCooldown = true;
-            const Http = new XMLHttpRequest();
-            Http.open("GET", "https://api.rally.io/v1/creator_coins/"+fieldData.creatorCoinSymbol+"/price");
-            Http.send();
 
-            Http.onreadystatechange = (e) => {
-                if (Http.readyState == 4 && Http.status == 200) {
-                    let data = JSON.parse(Http.responseText);
-                    console.log(data);
-                    sayMessage(String(data.symbol) + ": $" + data.priceInUSD.toString() + " RLY: " + data.priceInRLY.toString());
-                    setTimeout(function(){creatorCoinPriceCommandCooldown = false;},{creatorCoinPriceCommandCooldown}*1000);
+        if (o.detail.event.data.text == fieldData.creatorCoinPriceCommand && 0 == creatorCoinPriceCommandCooldown) {
+            creatorCoinPriceCommandCooldown = !0;
+            const o = new XMLHttpRequest;
+            o.open("GET", "https://api.rally.io/v1/creator_coins/" + fieldData.creatorCoinSymbol + "/price"), o.send(), o.onreadystatechange = (t => {
+                if (4 == o.readyState && 200 == o.status) {
+                    let t = JSON.parse(o.responseText);
+                    sayMessage(String(t.symbol) + ": $" + t.priceInUSD.toString() + " RLY: " + t.priceInRLY.toString()), setTimeout(function() {
+                        creatorCoinPriceCommandCooldown = !1
+                    }, 1e3 * {
+                        creatorCoinPriceCommandCooldown: creatorCoinPriceCommandCooldown
+                    })
                 }
+            })
+        }
 
-            }
-    }
-if (obj.detail.event.data.text == fieldData.creatorCoin && creatorCoinCooldown == false) {
-        creatorCoinCooldown = true;
-            const Http = new XMLHttpRequest();
-            Http.open("GET", "https://api.rally.io/v1/creator_coins/"+fieldData.creatorCoinSymbol+"/summary");
-            Http.send();
-
-            Http.onreadystatechange = (e) => {
-                if (Http.readyState == 4 && Http.status == 200) {
-                    let data = JSON.parse(Http.responseText);
-                    console.log(data);
-                    sayMessage("Supporters - " + data.totalSupporters+', Transactions - ' + data.totalTransaction + ', Support Volume - ' + data.totalSupportVolume + ', Total Creator Coins - ' + data.totalCoins);
-                    setTimeout(function(){rallyPriceCommandCooldown = false;},{rallyPriceCommandCooldown}*1000);
+        if (o.detail.event.data.text == fieldData.creatorCoin && 0 == creatorCoinCooldown) {
+            creatorCoinCooldown = !0;
+            const o = new XMLHttpRequest;
+            o.open("GET", "https://api.rally.io/v1/creator_coins/" + fieldData.creatorCoinSymbol + "/summary"), o.send(), o.onreadystatechange = (t => {
+                if (4 == o.readyState && 200 == o.status) {
+                    let t = JSON.parse(o.responseText);
+                    sayMessage("Supporters - " + t.totalSupporters + ", Transactions - " + t.totalTransaction + ", Support Volume - " + t.totalSupportVolume + ", Total Creator Coins - " + t.totalCoins), setTimeout(function() {
+                        rallyPriceCommandCooldown = !1
+                    }, 1e3 * {
+                        rallyPriceCommandCooldown: rallyPriceCommandCooldown
+                    })
                 }
-    }
-}
-if (obj.detail.event.data.text == fieldData.creatorCoinSupportersCommand && creatorCoinSupportersCommandCooldown == false) {
-            creatorCoinSupportersCommandCooldown = true;  
-        const Http = new XMLHttpRequest();
-            Http.open("GET", "https://api.rally.io/v1/creator_coins/"+fieldData.creatorCoinSymbol+"/summary");
-            Http.send();
+            })
+        }
 
-            Http.onreadystatechange = (e) => {
-                if (Http.readyState == 4 && Http.status == 200) {
-                    let data = JSON.parse(Http.responseText);
-                    console.log(data);
-                    sayMessage("Total Supporters: " + data.totalSupporters.toString());
-                    setTimeout(function(){creatorCoinSupportersCommandCooldown = false;},{creatorCoinSupportersCommandCooldown}*1000);
+        if (o.detail.event.data.text == fieldData.creatorCoinSupportersCommand && 0 == creatorCoinSupportersCommandCooldown) {
+            creatorCoinSupportersCommandCooldown = !0;
+            const o = new XMLHttpRequest;
+            o.open("GET", "https://api.rally.io/v1/creator_coins/" + fieldData.creatorCoinSymbol + "/summary"), o.send(), o.onreadystatechange = (t => {
+                if (4 == o.readyState && 200 == o.status) {
+                    let t = JSON.parse(o.responseText);
+                    sayMessage("Total Supporters: " + t.totalSupporters.toString()), setTimeout(function() {
+                        creatorCoinSupportersCommandCooldown = !1
+                    }, 1e3 * {
+                        creatorCoinSupportersCommandCooldown: creatorCoinSupportersCommandCooldown
+                    })
                 }
+            })
+        }
 
-            }
-    }if (obj.detail.event.data.text == fieldData.creatorCoinSupportVolumeCommand && creatorCoinSupportVolumeCommandCooldown == false) {
-        creatorCoinSupportVolumeCommandCooldown = true;
-            const Http = new XMLHttpRequest();
-            Http.open("GET", "https://api.rally.io/v1/creator_coins/"+fieldData.creatorCoinSymbol+"/summary");
-            Http.send();
-
-            Http.onreadystatechange = (e) => {
-                if (Http.readyState == 4 && Http.status == 200) {
-                    let data = JSON.parse(Http.responseText);
-                    console.log(data);
-                    sayMessage("Total Support Volume: " + data.totalSupportVolume.toString());
-                    setTimeout(function(){creatorCoinSupportVolumeCommandCooldown = false;},{creatorCoinSupportVolumeCommandCooldown}*1000);
+        if (o.detail.event.data.text == fieldData.creatorCoinSupportVolumeCommand && 0 == creatorCoinSupportVolumeCommandCooldown) {
+            creatorCoinSupportVolumeCommandCooldown = !0;
+            const o = new XMLHttpRequest;
+            o.open("GET", "https://api.rally.io/v1/creator_coins/" + fieldData.creatorCoinSymbol + "/summary"), o.send(), o.onreadystatechange = (t => {
+                if (4 == o.readyState && 200 == o.status) {
+                    let t = JSON.parse(o.responseText);
+                    sayMessage("Total Support Volume: " + t.totalSupportVolume.toString()), setTimeout(function() {
+                        creatorCoinSupportVolumeCommandCooldown = !1
+                    }, 1e3 * {
+                        creatorCoinSupportVolumeCommandCooldown: creatorCoinSupportVolumeCommandCooldown
+                    })
                 }
+            })
+        }
 
-            }
-    }
-if (obj.detail.event.data.text == fieldData.creatorCoinCountCommand && creatorCoinCountCommandCooldown == false) {
-    creatorCoinCountCommandCooldown = true;    
-        const Http = new XMLHttpRequest();
-            Http.open("GET", "https://api.rally.io/v1/creator_coins/"+fieldData.creatorCoinSymbol+"/summary");
-            Http.send();
-
-            Http.onreadystatechange = (e) => {
-                if (Http.readyState == 4 && Http.status == 200) {
-                    let data = JSON.parse(Http.responseText);
-                    console.log(data);
-                    sayMessage("Total Creator Coin Count: " + data.totalCoins.toString());
-                    setTimeout(function(){creatorCoinCountCommandCooldown = false;},{creatorCoinCountCommandCooldown}*1000);
+        if (o.detail.event.data.text == fieldData.creatorCoinCountCommand && 0 == creatorCoinCountCommandCooldown) {
+            creatorCoinCountCommandCooldown = !0;
+            const o = new XMLHttpRequest;
+            o.open("GET", "https://api.rally.io/v1/creator_coins/" + fieldData.creatorCoinSymbol + "/summary"), o.send(), o.onreadystatechange = (t => {
+                if (4 == o.readyState && 200 == o.status) {
+                    let t = JSON.parse(o.responseText);
+                    sayMessage("Total Creator Coin Count: " + t.totalCoins.toString()), setTimeout(function() {
+                        creatorCoinCountCommandCooldown = !1
+                    }, 1e3 * {
+                        creatorCoinCountCommandCooldown: creatorCoinCountCommandCooldown
+                    })
                 }
+            })
+        }
 
-            }
-    }
-    if (obj.detail.event.data.text == fieldData.creatorCoinTransactionsCommand && creatorCoinTransactionsCommandCooldown == false) {
-        creatorCoinTransactionsCommandCooldown = true;
-            const Http = new XMLHttpRequest();
-            Http.open("GET", "https://api.rally.io/v1/creator_coins/SKOT/summary");
-            Http.send();
-
-            Http.onreadystatechange = (e) => {
-                if (Http.readyState == 4 && Http.status == 200) {
-                    let data = JSON.parse(Http.responseText);
-                    console.log(data);
-                    sayMessage("Total Transactions: " + data.totalTransaction.toString());
-                    setTimeout(function(){creatorCoinTransactionsCommandCooldown = false;},{creatorCoinTransactionsCommandCooldown}*1000);
+        if (o.detail.event.data.text == fieldData.creatorCoinTransactionsCommand && 0 == creatorCoinTransactionsCommandCooldown) {
+            creatorCoinTransactionsCommandCooldown = !0;
+            const o = new XMLHttpRequest;
+            o.open("GET", "https://api.rally.io/v1/creator_coins/SKOT/summary"), o.send(), o.onreadystatechange = (t => {
+                if (4 == o.readyState && 200 == o.status) {
+                    let t = JSON.parse(o.responseText);
+                    sayMessage("Total Transactions: " + t.totalTransaction.toString()), setTimeout(function() {
+                        creatorCoinTransactionsCommandCooldown = !1
+                    }, 1e3 * {
+                        creatorCoinTransactionsCommandCooldown: creatorCoinTransactionsCommandCooldown
+                    })
                 }
+            })
         }
     }
 });
